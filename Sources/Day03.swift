55163eb4b93a616e4a9bd53aa14de6582f3cf99d
Yuriy Belikov <yuriybelikov1@gmail.com>
2025-04-20 00:37:58 +0300

    Day 3 solution

--- Sources/Day03.swift
+++ Sources/Day03.swift
@@ -12,20 +12,62 @@ struct Day03: AdventDay {
   var data: String
   
   func part1() -> Any {
+    processString(data)
+  }
+  
+  // Replace this with your solution for the second part of the day's challenge.
+  func part2() -> Any {
+    let allowCalcCommand = "do()"
+    let disallowCalcCommand = "don't()"
+    var windowWidth = disallowCalcCommand.count
+    var windowStartIndex = data.startIndex
+    var windowEndIndex = data.index(windowStartIndex, offsetBy: disallowCalcCommand.count - 1, limitedBy: data.endIndex) ?? data.endIndex
+    var lastUsedIndex = data.startIndex
+    var searchingForDont = true
+    var sum = 0
+    while windowEndIndex < data.endIndex {
+      if searchingForDont {
+        if String(data[windowStartIndex...windowEndIndex]) == disallowCalcCommand {
+          searchingForDont = false
+          sum += processString(String(data[lastUsedIndex...windowStartIndex]))
+          windowWidth = allowCalcCommand.count
+          windowEndIndex = data.indexShifted(from: windowStartIndex, offsetBy: windowWidth - 1)
+        }
+      } else {
+        if String(data[windowStartIndex...windowEndIndex]) == allowCalcCommand {
+          searchingForDont = true
+          lastUsedIndex =  data.indexShifted(from: windowEndIndex, offsetBy: 1)
+          windowWidth = disallowCalcCommand.count
+          windowEndIndex = data.indexShifted(from: windowStartIndex, offsetBy: windowWidth - 1)
+        }
+      }
+      windowStartIndex = data.index(after: windowStartIndex)
+      windowEndIndex = data.index(after: windowEndIndex)
+    }
+    if searchingForDont {
+      sum += processString(String(data[lastUsedIndex...]))
+    }
+    return sum
+  }
+  
+  func processString(_ str: String) -> Int {
     guard let mulOpRegex = try? Regex("mul\\(\\d+,\\d+\\)") else {
       return -1
     }
-    let matches = data.matches(of: mulOpRegex)
+    let matches = str.matches(of: mulOpRegex)
     let partialResults = matches.map { regexMatch in
-      let splitMulOp = String(data[regexMatch.range]).split(separator: ",")
+      let splitMulOp = String(str[regexMatch.range]).split(separator: ",")
       let firstMul = Int(splitMulOp[0].split(separator: "(")[1]) ?? 0
       let secondMul = Int(splitMulOp[1].split(separator: ")")[0]) ?? 0
       return firstMul * secondMul
     }
-  	return partialResults.reduce(0,+)
-  }
-  // Replace this with your solution for the second part of the day's challenge.
-  func part2() -> Any {
-    return 0
+    return partialResults.reduce(0,+)
   }
 }
+
+extension String {
+  func indexShifted(from index: String.Index, offsetBy distance: Int) -> String.Index {
+    return self.index(index, offsetBy: distance, limitedBy: self.endIndex) ?? endIndex
+  }
+}
+
--- Tests/Day03.swift
+++ Tests/Day03.swift
@@ -13,17 +13,52 @@ import Testing
 // pass.
 struct Day03Tests {
   // Smoke test data provided in the challenge question
-  let testData = """
+  let testData1 = """
       xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
     """
-
+  
+  let testData2_1 = """
+                  xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+                  """
+  
+  let testData2_2 = """
+                  xmul(2,4)&mul[3,7]!
+                  """
+  
+  let testData2_3 = """
+                  xmul(2,4)&mul[3,7]don't()_mul(4,6)don't()?mul(4,3)!?do()mul(11,2)!
+                  """
+  
+  let testData2_4 = """
+                  xmul(2,4)&mul[3,7]do()mul(4,6)don't()?mul(4,3)!?do()mul(11,2)!
+                  """
+  
+  let testData2_5 = """
+                  xmul(2,4)&mul[3,7]do()mul(4,6)don't()?mul(4,3)!?do()mul(11,2)!
+                  """
+  
   @Test func testPart1() async throws {
-    let challenge = Day03(data: testData)
+    let challenge = Day03(data: testData1)
     #expect(String(describing: challenge.part1()) == "161")
   }
 
-  @Test func testPart2() async throws {
-    let challenge = Day03(data: testData)
-    #expect(String(describing: challenge.part2()) == "0")
+  @Test func testPart2_1() async throws {
+    let challenge = Day03(data: testData2_1)
+    #expect(String(describing: challenge.part2()) == "48")
+  }
+  
+  @Test func testPart2_2() async throws {
+    let challenge = Day03(data: testData2_2)
+    #expect(String(describing: challenge.part2()) == "8")
+  }
+  
+  @Test func testPart2_3() async throws {
+    let challenge = Day03(data: testData2_3)
+    #expect(String(describing: challenge.part2()) == "30")
+  }
+  
+  @Test func testPart2_4() async throws {
+    let challenge = Day03(data: testData2_4)
+    #expect(String(describing: challenge.part2()) == "54")
   }
 }
